0058c74617a1d3d6eb14ed93750a2c0b9ae8de15
Caaki <95718662+Caaki@users.noreply.github.com>
2023-06-23 21:14:31 +0200

    Add files via upload

---
+++ dz10/DZ10/src/main/java/com/metropolitan/service/TodoService.java
@@ -0,0 +1,17 @@
+package com.metropolitan.service;
+
+import com.metropolitan.model.Todo;
+import java.util.List;
+
+public interface TodoService {
+
+    List<Todo> listTodos();
+
+    void save(Todo todo);
+
+    void complete(long id);
+
+    void remove(long id);
+
+    Todo findById(long id);
+}
